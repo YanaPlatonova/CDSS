210d7dd046a9c0d0da123b5c5552d419d2be7e6b
Sergey G <sergey@v4scale.com>
2020-06-08 18:52:33 +0300

    tree

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,41 @@
-console.log("Start");
+var tree = [
+    {
+        text: "Parent 1",
+        nodes: [
+            {
+                text: "Child 1",
+                nodes: [
+                    {
+                        text: "Grandchild 1"
+                    },
+                    {
+                        text: "Grandchild 2"
+                    }
+                ]
+            },
+            {
+                text: "Child 2"
+            }
+        ]
+    },
+    {
+        text: "Parent 2"
+    },
+    {
+        text: "Parent 3"
+    },
+    {
+        text: "Parent 4"
+    },
+    {
+        text: "Parent 5"
+    }
+];
+
+function getTree() {
+    return tree;
+}
+
+$(document).ready(function () {
+    $('#tree').treeview({ data: getTree() });
+});
